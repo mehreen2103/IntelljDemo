aac71f6d774a9ff6a650ff9e4310efb62528ba28
mehreen2103 <mehreenansari2097@gmail.com>
2025-09-08 12:54:29 +0530

    done

---
+++ src/in/co/rays/basic/Animal.java
@@ -0,0 +1,11 @@
+package in.co.rays.basic;
+
+public class Animal {
+    String name;
+
+    public void eat() {
+
+        System.out.println(name + " is eating.");
+    }
+}
+
---
+++ src/in/co/rays/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package in.co.rays.basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.name = "Tommy";
+        dog.eat();
+        dog.bark();
+    }
+}
